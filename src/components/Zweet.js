eeb36d002e0806214ea98f1bef84bda1e5bd9f85
zeriong <jaeryong95@gmail.com>
2022-11-12 23:56:24 +0900

    zweet업로드기능을 컴포넌트로 추가

---
+++ src/components/Zweet.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Zweet = ({ zweetObj, isOwner })=> (
+        <>
+            <div>
+                <h4>{zweetObj.text}</h4>
+                {isOwner && (
+                    <>
+                        <button>Delete Zweet</button>
+                        <button>Edit Zweet</button>
+                    </>
+                )}
+            </div>
+        </>
+);
