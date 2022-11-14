b479c9810e8fa23af679c107b984bf72adba7d18
zeriong <jaeryong95@gmail.com>
2022-11-15 00:15:14 +0900

    Zweet컴포넌트에 글 삭제, 수정기능 추가

--- src/components/Zweet.js
+++ src/components/Zweet.js
@@ -1,16 +1,60 @@
-import React from "react";
+import React, {useState} from "react";
+import {dbService} from "../Fbase";
+import {deleteDoc, doc, updateDoc } from "firebase/firestore";
 
 
-export const Zweet = ({ zweetObj, isOwner })=> (
-        <>
-            <div>
-                <h4>{zweetObj.text}</h4>
-                {isOwner && (
+export const Zweet = ({ zweetObj, isOwner })=> {
+    const [editing, setEditing] = useState(false);
+    const [newZweet, setNewZweet] = useState(zweetObj.text);
+    const zweetTextRef = doc(dbService, "zweets", `${zweetObj.id}`);
+    const onDeleteClick = async () => {
+        const ok = window.confirm("Are you sure you want to delete this zweet?");
+        if(ok){
+            await deleteDoc(zweetTextRef);
+        }
+    };
+    const toggleEditing = () => setEditing((prev)=> !prev);
+    const onChange = ({target:{value}})=> setNewZweet(value);
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        await updateDoc(zweetTextRef,{
+            text: newZweet,
+        });
+        setEditing(false);
+    };
+    return (
+        <div>
+            {
+                editing ? (
                     <>
-                        <button>Delete Zweet</button>
-                        <button>Edit Zweet</button>
+                        <form onSubmit={onSubmit}>
+                            <input type="text"
+                                   placeholder="Edit your zweet"
+                                   value={newZweet}
+                                   required
+                                   onChange={onChange}
+                            />
+                            <input
+                                type="submit"
+                                value="Update Zweet"
+                            />
+                        </form>
+                        <button onClick={toggleEditing}>Cancel</button>
                     </>
-                )}
-            </div>
-        </>
-);
+                ) : (
+                    <>
+                        <div>
+                            <h4>{zweetObj.text}</h4>
+                            {isOwner && (
+                                <>
+                                    <button onClick={onDeleteClick}>Delete Zweet</button>
+                                    <button onClick={toggleEditing}>Edit Zweet</button>
+                                </>
+                            )}
+                        </div>
+                    </>
+                )
+            }
+        </div>
+    );
+};
