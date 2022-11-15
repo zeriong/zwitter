e3d69952d64121b6c675c661bf4c2492e4c1b2c5
zeriong <jaeryong95@gmail.com>
2022-11-16 00:42:07 +0900

    프로필 기능 진행중...

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,32 @@
-import React from "react";
-import {authService} from "Fbase";
+import React, { useEffect } from "react";
+import {authService, dbService} from "Fbase";
 import { useNavigate } from "react-router-dom";
+import {collection, getDocs, query, where} from "@firebase/firestore";
 
-export const Profile = ()=> {
+export const Profile = ({userObj})=> {
     const navigate = useNavigate();
     const onLogOutClick = () => {
         authService.signOut();
         navigate("/",{replace:true});
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const getMyZweets = async ()=> {
+        try {
+            const q = query(
+                collection(dbService,"zweets"),
+                where("creatorId", "==", userObj.uid),
+            );
+            const querySnapshot = await getDocs(q);
+            querySnapshot.forEach((doc)=>{
+                console.log(doc.id,"=>",doc.data());
+            });
+        } catch (e) {
+            console.log(e);
+        }
+    }
+    useEffect(()=> {
+        getMyZweets();
+    },[getMyZweets])
     return (
         <>
             <button onClick={onLogOutClick}>Log Out</button>
