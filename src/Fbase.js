4c02fc4fa5627a7f247997f409a222ae6ba31311
zeriong <jaeryong95@gmail.com>
2022-11-16 00:40:00 +0900

    storageService변수 추가

--- src/Fbase.js
+++ src/Fbase.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
@@ -15,3 +16,4 @@ firebase.initializeApp(firebaseConfig);
 
 export const authService = firebase.auth();
 export const dbService = getFirestore();
+export const storageService = getStorage();
