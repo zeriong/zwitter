a9a3bd074cbe047643250d35919d8c7285948252
zeriong <jaeryong95@gmail.com>
2022-11-17 00:07:01 +0900

    userObj스테이트 변경(실시간 프로필명 변경을 위함)

--- src/components/App.js
+++ src/components/App.js
@@ -10,16 +10,36 @@ function App() {
     authService.onAuthStateChanged((user)=>{
       if (user) {
         setIsloggedIn(true);
-        setUserObj(user);
+        setUserObj({
+          displayName: user.displayName,
+          uid: user.uid,
+          updateProfile: { displayName: user.displayName },
+        });
       } else {
         setIsloggedIn(false);
       }
       setInit(true);
     });
       },[]);
+  const refreshUser = () => {
+    const user = authService.currentUser;
+    setUserObj({
+      displayName: user.displayName,
+      uid: user.uid,
+      updateProfile: { displayName: user.displayName },
+    });
+  }
   return (
       <>
-        { init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} /> : "Initializing...." }
+        { init ? (
+            <AppRouter
+                isLoggedIn={isLoggedIn}
+                userObj={userObj}
+                refreshUser={refreshUser}
+            />
+        ) : (
+            "Initializing...."
+        ) }
       </>
   );
 }
