4828545eeea63ab80d69a2d8a550a5450ce6f09f
zeriong <jaeryong95@gmail.com>
2022-11-17 23:38:21 +0900

    Route path 변경

--- src/components/Router.js
+++ src/components/Router.js
@@ -22,8 +22,8 @@ export const AppRouter = ( {refreshUser, isLoggedIn, userObj} )=> {
                             justifyContent: "center",
                         }}
                     >
-                        <Route path="/" element={<Home userObj={userObj}/>}/>
-                        <Route path="/Profile"
+                        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home userObj={userObj}/>}/>
+                        <Route path={`${process.env.PUBLIC_URL}/profile`}
                                element={ <Profile userObj={userObj}
                                 refreshUser={refreshUser} /> }
                         />
